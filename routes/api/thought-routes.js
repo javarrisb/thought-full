382aa2b774570e8c0d22fb32aa2c324f74d8d217
Javarris Barnett <javarrisb@gmail.con>
2022-02-18 21:23:13 +0100

    added thought routes

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,35 @@
+const router = require('express').Router();
+const {
+    getAllThought,
+    getThoughtById,
+    addThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction
+} = require('../../controllers/thought-controller');
+
+// /api/thoughts
+router 
+    .route('/')
+    .get(getAllThought)
+    .post(addThought);
+
+// /api/:thoughtId
+router
+    .route('/:thoughtId')
+    .get(getThoughtById)
+    .put(updateThought)
+    .delete(deleteThought);
+
+// /api/:thoughtId/reactions
+router
+    .route('/:thoughtId/reactions')
+    .post(addReaction);
+
+// /api/:thoughtId/reactions/:reactionId
+router
+    .route('/:thoughtId/reactions/:reactionId')
+    .delete(deleteReaction);
+
+module.exports = router;
